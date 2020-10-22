79aa0e924de59b3d3e119ff5708d442bb12e8c57
praveen-raghav <54539949+praveen-raghav@users.noreply.github.com>
2020-10-22 18:15:42 +0530

    Update mail_job.rb

--- app/jobs/mail_job.rb
+++ app/jobs/mail_job.rb
@@ -6,7 +6,7 @@ class MailJob < ActiveJob::Base
     post_id = @response[:post_id]
     @users = User.joins("INNER JOIN post_follows ON users.id = post_follows.user_id").where("post_follows.post_id="+post_id.to_s)
     post_title = Post.find(post_id)[:title]
-
+# merge PR
     @users.each do |eachuser|
 
       #if the user replying also follows post, do not send mail this particular user
